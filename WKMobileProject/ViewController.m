2ec0cacb55379cf15c8374f151f5cf1caafc9680
mwi01 <2605072548@qq.com>
2017-08-24 10:28:46 +0800

    修改老分枝

--- WKMobileProject/ViewController.m
+++ WKMobileProject/ViewController.m
@@ -8,25 +8,26 @@
 
 #import "ViewController.h"
 #import "WKHeader.h"
-@interface ViewController ()<CTAPIManagerParamSource, CTAPIManagerCallBackDelegate>
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
-    @property (nonatomic,strong) WKAPIManager *mAPIManager;
-    
+@property (weak, nonatomic) IBOutlet UITableView *mTableView;
+
 @end
 
 @implementation ViewController
+@synthesize mTableView;
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:YES];
+}
 
-    - (void)viewWillAppear:(BOOL)animated{
-        [super viewWillAppear:YES];
-    }
-    
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.title = @"首页";
     
-   _mAPIManager.mAPIName =  @"/home/banner";
-    [self.mAPIManager loadData];
-
+    mTableView.delegate = self;
+    mTableView.dataSource = self;
+    self.view.backgroundColor = [UIColor colorWithRed:0.949019607843137 green:0.949019607843137 blue:0.949019607843137 alpha:1.00];
 }
 
 
@@ -34,42 +35,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-#pragma mark - CTAPIManagerParamSource
-- (NSDictionary *)paramsForApi:(CTAPIBaseManager *)manager{
-    NSDictionary *params = @{};
-    
-    if (manager == self.mAPIManager) {
-        params = nil;
-    }
-    
-    return params;
-
+- (NSInteger)numberOfRowsInSection:(NSInteger)section{
+    return 0;
 }
-#pragma mark - CTAPIManagerCallBackDelegate
-- (void)managerCallAPIDidSuccess:(CTAPIBaseManager *)manager
-    {
-        if (manager == self.mAPIManager) {
-            NSLog(@"%@", [manager fetchDataWithReformer:nil]);
-        }
-    }
-    
-- (void)managerCallAPIDidFailed:(CTAPIBaseManager *)manager
-    {
-        if (manager == self.mAPIManager) {
-            NSLog(@"%@", [manager fetchDataWithReformer:nil]);
-        }
-    }
-    
-#pragma mark - getters and setters
-- (WKAPIManager *)mAPIManager
-    {
-        if (_mAPIManager == nil) {
-            _mAPIManager = [[WKAPIManager alloc] init];
-            _mAPIManager.delegate = self;
-            _mAPIManager.paramSource = self;
-        }
-        return _mAPIManager;
-    }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld行",indexPath.row];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 45;
+}
 @end
